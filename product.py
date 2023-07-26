51af73b72209fcc4f048e72d9ca786d453bedd60
Olivia Wu <oliviawu97625@gmail.com>
2023-07-26 17:09:50 +0800

    refactor

--- product.py
+++ product.py
@@ -1,33 +1,56 @@
 # 二維記帳小程式
 
-# 讀取csv檔案+Split
+
 import os # operating systems
-items = []
-if os.path.isfile('products.csv'): # 先檢查檔案是否存在
-	print('Find the "products.csv" file.')
-	with open('products.csv', 'r', encoding='utf-8') as f:
+
+# 讀取csv檔案+Split
+def readfile(filename):
+	items = []
+	with open(filename, 'r', encoding='utf-8') as f:
 		for item in f:
 			if 'Item, Cost' in item:
 				continue
 			name, price = item.strip().split(',')
 			items.append([name, price])
-else:
-	print('Could not find the file.')
-while True:
-	name = input('Please enter the name of the item: ')
-	if name == 'q':
-		break
-	price = input('Please enter the cost of the item: ')
-	items.append([name, price])
-print(items) 
+	return items
+
+
+#讓使用者輸入
+def userinput(items):
+	while True:
+		name = input('Please enter the name of the item: ')
+		if name == 'q':
+			break
+		price = input('Please enter the cost of the item: ')
+		items.append([name, price])
+	print(items) 
+	return items
 
 # 把所有購買紀錄印出來
-for item in items:
-	print(item)
-	print(item[0], 'price is', item[1])
+def printitems(items):
+	for item in items:
+		print(item)
+		print(item[0], 'price is', item[1])
 
 #寫入檔案到csv
-with open('products.csv', 'w', encoding='utf-8') as f: # 為了避免有中文的亂碼
-	f.write('Item, Cost\n')
-	for item in items:
-		f.write(item[0] + ',' + item[1] + '\n' )
+def writefile(filename, items):
+	with open(filename, 'w', encoding='utf-8') as f: # 為了避免有中文的亂碼
+		f.write('Item, Cost\n')
+		for item in items:
+			f.write(item[0] + ',' + item[1] + '\n' )
+
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename): # 先檢查檔案是否存在
+		print('Find the file.')
+		items = readfile(filename)
+	else:
+		print('Could not find the file.')
+
+	items = userinput(items)
+	printitems(items)
+	writefile('products.csv', items)
+
+
+main()
